f0434ea8fb66c77d8a9303b6f2be09e052d85f2f
Ehsanul Mostafa Imon <mostafaimon012020@gmail.com>
2022-06-25 15:21:21 +0600

    fcfs done

---
+++ FCFS.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	cout<<"---First come first serve scheduling algorithm---"<<endl;
+	int p[100] ={0};
+	int At[100]={0},bt[100]={0},ct[100]={0},tt[100]={0},wt[100]={0};
+	int n,i,j,temp,sum=0;
+	float avg_tt=0,avg_wt=0;
+
+	cout<<"Enter array size: ";
+	cin>>n;
+	cout<<"\nEnter process sequence:"<<endl;
+	for(i=0;i<n;i++){
+    cin>>p[i];
+	}
+    cout<<"\nEnter Arrival Time: (AT)"<<endl;
+	for(i=0;i<n;i++){
+    cin>>At[i];
+	}
+	cout<<"\nEnter Burst Time: (BT)"<<endl;
+	for(i=0;i<n;i++){
+    cin>>bt[i];
+	}
+
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(At[i]>At[j]){
+
+				temp = At[i];
+				At[i]=At[j];
+				At[j]=temp;
+
+					temp = bt[i];
+				bt[i]=bt[j];
+				bt[j]=temp;
+
+					temp = p[i];
+				p[i]=p[j];
+				p[j]=temp;
+			}
+		}
+	}
+	//calculate completion time of the process
+	for(i=0;i<n;i++){
+		sum=sum+bt[i];
+		ct[i]+=sum;
+	}
+	//calculate turnaround time and waiting time
+	for(i=0;i<n;i++){
+		tt[i]=ct[i]-At[i];
+		avg_tt+=tt[i];
+
+		wt[i]=tt[i]-bt[i];
+		avg_wt+=wt[i];
+	}
+	cout<<"Solution  "<<endl;
+	cout<<"Process\tAT\tBT\tCT\tTT\tWT" <<endl;
+	for(i=0;i<n;i++){
+	cout<<"p"<<p[i]<<"\t"<<At[i]<<"\t"<<bt[i]<<"\t"<<ct[i]<<"\t"<<tt[i]<<"\t"<<wt[i]<<endl;
+	}
+	cout<<"Average turnaround time ="<<avg_tt<<endl;
+	cout<<"Average waiting time ="<<avg_wt<<endl;
+    return 0;
+}
+
